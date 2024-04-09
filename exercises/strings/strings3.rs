ffc7bf1ba04f903897847371e8d873008e8ee436
kylinary <2032153705@qq.com>
2024-04-09 21:12:32 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,37 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+
+    let mut index = 0;
+    for c in input.as_bytes() {
+        if *c == ' ' as u8 {
+            index += 1;
+        } else {
+            break
+        }
+    }
+    let mut index2 = input.len();
+    for c in input.chars().rev() {
+        if c == ' ' {
+            index2 -= 1
+        } else {
+            break
+        }
+    }
+    input[index..index2].to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_owned() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
